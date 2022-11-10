78fc3b245dec0072d2e7e6a86a39438f96c90607
Arslan <rslnkrmt2552@gmail.com>
2022-11-10 05:33:57 +0500

    update gql uri

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GlobalStyles from "./styles";
-import Pages from "./pages";
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import React from "react"
+import ReactDOM from "react-dom"
+import GlobalStyles from "./styles"
+import Pages from "./pages"
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: "https://server-catstronauts.herokuapp.com/", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-part5-server-production-894a.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
-});
+})
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById("root")
-);
+  document.getElementById("root"),
+)
